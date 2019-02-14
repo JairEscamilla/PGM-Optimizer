bf19b21af10088eeec8507f18850089828f71801
Jair Escamilla <hernandez_yo14@outlook.es>
2019-02-13 23:05:37 -0600

    Desplegando datos

--- pgm.c
+++ pgm.c
@@ -1,10 +1,19 @@
-#include <stdio.h> // Incluyo la libreria
+// Incluyo las librerias
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 // Prototipos de las funciones
-
+void desplegarDatos();
 // Funcion principal
 int main(){
-
+  desplegarDatos();
   return 0;
 }
-
 // Desarrollo de las funciones
+void desplegarDatos(){
+  system("clear");
+  printf("Desarrollador: Allan Jair Escamilla Hernández\nPresione una tecla para continuar...");
+  getchar();
+  system("clear");
+  printf("Objetivo: Procesar digitalmente una imagen PGM dada por el usuario.\n");
+}
