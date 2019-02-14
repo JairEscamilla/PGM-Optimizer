92bfc9db2ed6009b6970981b394db1dd4cea0320
Jair Escamilla <hernandez_yo14@outlook.es>
2019-02-13 23:17:17 -0600

    Validando la existencia de archivos

--- pgm.c
+++ pgm.c
@@ -4,9 +4,14 @@
 #include <stdlib.h>
 // Prototipos de las funciones
 void desplegarDatos();
+void menu();
+void PedirArchivo(char[]);
+void ValidarArchivo(char[], int*);
+void obtenerDimensiones(char[], int*, int*);
 // Funcion principal
 int main(){
   desplegarDatos();
+  menu();
   return 0;
 }
 // Desarrollo de las funciones
@@ -16,4 +21,29 @@ void desplegarDatos(){
   getchar();
   system("clear");
   printf("Objetivo: Procesar digitalmente una imagen PGM dada por el usuario.\n");
+}
+
+void menu(){
+  int opcion = 0, validacion, Ancho, Alto;
+  char NombreArchivo[200];
+  PedirArchivo(NombreArchivo);
+  ValidarArchivo(NombreArchivo, &validacion);
+  if (validacion == 0) {
+    obtenerDimensiones(NombreArchivo, *Ancho, *Alto);
+  }else{
+    printf("No se ha podido abrir tu archivo, vuelve a intentarlo):\n");
+  }
+}
+void PedirArchivo(char Nombre[]){
+  printf("Ingresar nombre del archivo a procesar: ");
+  gets(Nombre);
+}
+void ValidarArchivo(char Nombre[], int* Validacion){
+  FILE* Archivo = fopen(Nombre, "rb");
+  if(Archivo == NULL)
+    *Validacion = 1;
+  else{
+    *Validacion = 0;
+    fclose(Archivo);
+  }
 }
