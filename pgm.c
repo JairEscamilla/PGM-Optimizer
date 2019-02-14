331d19af578cb8522bea8896ff80f2c1fb49fbf7
Jair Escamilla <hernandez_yo14@outlook.es>
2019-02-14 15:21:42 -0600

    Ya se puede ecualizar una imagen

--- pgm.c
+++ pgm.c
@@ -9,7 +9,9 @@ void PedirArchivo(char[]);
 void ValidarArchivo(char[], int*);
 void obtenerDimensiones(char[], int*, int*);
 void cargarArchivo(char[], int, int, int*[]);
-void guardar(int**, int, int);
+void guardar(int**, int, int, char[]);
+void negativo(int**, int, int);
+void NuevoNombre(char[]);
 // Funcion principal
 int main(){
   desplegarDatos();
@@ -27,7 +29,7 @@ void desplegarDatos(){
 
 void menu(){
   int opcion = 0, validacion, Ancho, Alto, **Matriz;
-  char NombreArchivo[200];
+  char NombreArchivo[200], NuevoNombreA[200];
   PedirArchivo(NombreArchivo);
   ValidarArchivo(NombreArchivo, &validacion);
   if (validacion == 0) {
@@ -38,7 +40,33 @@ void menu(){
       Matriz[i] = (int*)malloc(Ancho*sizeof(int)); // Filas
     // Ahora voy a cargar la imagen a la matriz
     cargarArchivo(NombreArchivo, Ancho, Alto, Matriz);
-    guardar(Matriz, Ancho, Alto);
+    system("clear");
+    printf("\t\t\t\tMENU\n");
+    printf("\t1.- Obtener negativo de la imagen.\n");
+    printf("\t2.- Ecualizar la imagen.\n");
+    printf("\t3.- Reducir la imagen.\n");
+    printf("\t4.- Ampliar la imagen.\n\n");
+    printf("Elija una opcion-> ");
+    scanf("%d", &opcion);
+    switch (opcion) {
+      case 1:
+        negativo(Matriz, Ancho, Alto);
+        break;
+      case 2:
+        printf("Ecualizar\n");
+        break;
+      case 3:
+        printf("Reducir\n");
+        break;
+      case 4:
+        printf("Ampliar\n");
+        break;
+      default:
+        printf("Ingresa una opcion valida!\n");
+    }
+    NuevoNombre(NuevoNombreA);
+    guardar(Matriz, Ancho, Alto, NuevoNombreA);
+    printf("El proceso ha sido completado!\n");
   }else{
     printf("No se ha podido abrir tu archivo, vuelve a intentarlo):\n");
   }
@@ -76,8 +104,8 @@ void cargarArchivo(char Nombre[], int Ancho, int Alto, int* Matriz[]){
   }
   fclose(Archivo);
 }
-void guardar(int** Matriz, int Ancho, int Alto){
-  FILE* Archivo = fopen("prueba.raw", "wb");
+void guardar(int** Matriz, int Ancho, int Alto, char Nombre[]){
+  FILE* Archivo = fopen(Nombre, "wb");
   fprintf(Archivo, "P5 %d %d 255 ", Ancho, Alto);
   for(int i= 0; i < Alto; i++){
     for(int j = 0; j < Ancho; j++){
@@ -86,4 +114,16 @@ void guardar(int** Matriz, int Ancho, int Alto){
   }
 
   fclose(Archivo);
+}
+void negativo(int** Matriz, int Ancho, int Alto){
+  for(int i = 0; i < Alto; i++){
+    for(int j = 0; j < Ancho; j++){
+      Matriz[i][j] = Matriz[i][j]*(-1)+255;
+    }
+  }
+}
+void NuevoNombre(char Name[]){
+  printf("Ingresar nuevo nombre del archivo: ");
+  __fpurge(stdin);
+  gets(Name);
 }
